01c4ce5a24a2dc898331283316635c0e5789e61c
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-12-19 18:27:49 +0200

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -9,12 +16,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  
+
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(onPressed: _addDataToFirestore, child: Text("Click")),
         ),
       ),
     );
   }
+
+
 }
+  Future<void> _addDataToFirestore() async {
+    try {
+      final doc = FirebaseFirestore.instance.collection('users').doc('user1');
+      await doc.set({'name': 'Mohamed fareed', 'age': 30});
+      print('Data written to Firestore successfully!'); 
+    } catch (e) {
+      print('Error writing to Firestore: $e');
+    }
+  }
