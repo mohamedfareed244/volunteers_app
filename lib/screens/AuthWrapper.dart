6ae94cff8434500bca6fe2b3bfc0e70b1b1f8896
nnouran <96547311+nouranMo@users.noreply.github.com>
2024-12-25 21:14:36 +0400

    dynamic drawer

--- lib/screens/AuthWrapper.dart
+++ lib/screens/AuthWrapper.dart
@@ -6,17 +6,19 @@ import 'package:volunteers_app/views/Drawer/Drawer.dart';
 import 'package:volunteers_app/views/WelcomeScreen.dart';
 import 'package:volunteers_app/views/dashboard/organization_dashboard.dart';
 
-
-
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({super.key});
 
   Future<String?> _getUserRole(String userId) async {
-    final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    final userDoc =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
     if (userDoc.exists) {
       return 'user';
     }
-    final orgDoc = await FirebaseFirestore.instance.collection('Organization').doc(userId).get();
+    final orgDoc = await FirebaseFirestore.instance
+        .collection('Organization')
+        .doc(userId)
+        .get();
     if (orgDoc.exists) {
       return 'organization';
     }
@@ -36,22 +38,31 @@ class AuthWrapper extends StatelessWidget {
       future: _getUserRole(user.uid),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(
+              child: CircularProgressIndicator(
+            backgroundColor: Colors.white,
+          ));
         }
 
         final role = snapshot.data;
-        if (role == 'user') {
-          return  drawerr(); // Screen for regular users
-        } else if (role == 'organization') {
-          return  OrganizationDashboard(); // Screen for organizations
+
+        // if (role == 'user') {
+        //   return  drawerr(); // Screen for regular users
+        // } else if (role == 'organization') {
+        //   return  drawerr(); // Screen for organizations
+        // }
+
+        if (role == 'user' || role == 'organization') {
+          print("role: $role");
+          return drawerr(role:role!);
         }
 
-              // Role not recognized, log out the user
+        // Role not recognized, log out the user
         WidgetsBinding.instance.addPostFrameCallback((_) async {
           await FirebaseAuth.instance.signOut();
           Navigator.of(context).pushAndRemoveUntil(
             MaterialPageRoute(builder: (context) => const WelcomeScreen()),
-            (route) => false,//removes all the existing routes from the stack
+            (route) => false, //removes all the existing routes from the stack
           );
         });
 
